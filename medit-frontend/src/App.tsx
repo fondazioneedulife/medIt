847e69eed79a3f6fbe431a84afb58cb4cd8d0a10
DiegoM1111 <diegomilli03@gmail.com>
2025-02-11 12:40:00 +0100

    MDT-1: chore - insert routes and  base project folders

--- medit-frontend/src/App.tsx
+++ medit-frontend/src/App.tsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { config } from "./config";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter basename={config.APP_BASENAME}>
+        <Routes>
+          {/* <Route path="/" element={<Layout />}> */}
+          <Route path="/">
+            {/* TODO: set default route to redirect user first */}
+            {/* <Route index element={<Root />} /> */}
+
+            {/* TODO: set login guard */}
+            {/* <Route path="login" element={<LoginMainContext />}> */}
+            <Route path="login">
+              {/* all login route */}
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
