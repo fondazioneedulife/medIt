e69fb5788c39d54383ecd4fbf6940cc6c0067fdd
Talhaimran03 <imrantalha199@gmail.com>
2025-02-23 21:03:37 +0100

    MDT-50: feat - add Medication entity

---
+++ medit-backend/src/main/kotlin/medicare/medit/model/Medication.kt
@@ -0,0 +1,23 @@
+package medicare.medit.model
+
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "medications")
+data class Medication(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    val user: User,
+    val name: String,
+    val type: MedicationTypeEnum,
+    val dose: String,
+    val program: String,
+    val quantity: Int,
+    val note: String,
+    val createdAt: LocalDateTime,
+    val updatedAt: LocalDateTime,
+    val syncedAt: LocalDateTime
+)
