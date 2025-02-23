10c92a72b03b9fb93c350d31fc64e1774691de48
Talhaimran03 <imrantalha199@gmail.com>
2025-02-23 21:04:30 +0100

    MDT-50: feat - add TakenMedication entity

---
+++ medit-backend/src/main/kotlin/medicare/medit/model/TakenMedication.kt
@@ -0,0 +1,16 @@
+package medicare.medit.model
+
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "taken_medications")
+data class TakenMedication(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+    @ManyToOne
+    @JoinColumn(name = "reminder_id")
+    val reminder: Reminder,
+    val dateTime: LocalDateTime,
+    val syncedAt: LocalDateTime
+)
