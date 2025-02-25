10609014d7c1e547460e81cc67d6a941d0607d3a
davideantinori <davide.antinori@hotmail.it>
2025-02-25 12:34:37 +0100

    MDT-38: feat - add Dockerfile to frontend/backend , add docker-compose to publish , add script: start to package.json

--- medit-frontend/src/components/home/home.tsx
+++ medit-frontend/src/components/home/home.tsx
@@ -1,5 +1,5 @@
 import { Navbar } from "../Navbar/Navbar.tsx";
-import { MedicineComponent } from "./MedicineComponent/medicineComponent.tsx";
+import { MedicineComponent } from "./MedicineComponent/MedicineComponent.tsx";
 import { Calendar } from "./calendar/calendar.tsx";
 import "../../index.css";
 import { FilterButton } from "./FilterButton/FilterButton.tsx";
