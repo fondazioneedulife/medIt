9bc0344acea4d6ae35278e4b6f77f9f09809221d
Talhaimran03 <imrantalha199@gmail.com>
2025-03-01 15:31:11 +0100

    MDT-61: feat - update LoginLabel

--- medit-frontend/src/components/login/LoginLabel.tsx
+++ medit-frontend/src/components/login/LoginLabel.tsx
@@ -1,53 +1,86 @@
 import React from "react";
-import { Box, Stack, TextField } from "@mui/material";
+import { Box, ListItem } from "@mui/material";
+import TextField from "@mui/material/TextField";
 
-interface LabelProps {
+interface LoginLabelProps {
   inputName: string;
   placeholder: string;
   img: string;
   showHr?: boolean;
-  style?: React.CSSProperties;
-  iconshow?: boolean;
   type?: string;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
 }
 
-export const LoginLabel: React.FC<LabelProps> = ({
+export const LoginLabel: React.FC<LoginLabelProps> = ({
+  inputName,
   img,
   placeholder,
   showHr = true,
   type = "text",
+  onChange,
 }) => {
   return (
-    <Box sx={{ width: '100%' }}>
-        <Stack
-            direction="row"
-            alignItems="center"
-            spacing={2}
+    <>
+      <ListItem
+        alignItems="center"
+        style={{
+          justifyContent: "center",
+        }}
+      >
+        <img
+          src={img}
+          alt="Icon"
+          style={{ paddingRight: "20px", width: "2.2rem" }}
+        />
+        <Box
+          component="div"
+          sx={{ "& > :not(style)": { m: 1, width: "13rem" } }}
         >
-            <img
-                src={img}
-                alt="icon"
-                style={{
-                    height: '1.5rem',
-                    width: '1.5rem'
-                }}
-            />
-            <TextField
-                fullWidth
-                placeholder={placeholder}
-                type={type}
-                variant="standard"
-                InputProps={{ disableUnderline: true }}
-            />
-        </Stack>
-        {showHr && <hr style={{
-            width: '100%',
-            marginTop: '0.5rem',
-            color: 'black',
-            backgroundColor: 'black',
-            border: 'none',
-            height: '1px'
-        }} />}
-    </Box>
+          <TextField
+            name={inputName}
+            placeholder={placeholder}
+            variant="outlined"
+            type={type}
+            onChange={onChange}
+            InputProps={{
+              style: {
+                backgroundColor: "transparent",
+                border: "none",
+                outline: "none",
+                width: "100%",
+                height: "3rem",
+                fontSize: "1.2rem",
+                color: "black",
+                fontWeight: "600",
+              },
+            }}
+            InputLabelProps={{
+              style: {
+                color: "black",
+              },
+            }}
+            sx={{
+              "& .MuiOutlinedInput-root": {
+                "& fieldset": {
+                  borderColor: "transparent",
+                },
+                "&:hover fieldset": {
+                  borderColor: "transparent",
+                },
+                "&.Mui-focused fieldset": {
+                  borderColor: "transparent",
+                },
+                "& input": {
+                  color: "black", // Assicurati che il colore del testo sia nero
+                },
+              },
+            }}
+          />
+        </Box>
+      </ListItem>
+      {showHr === true && (
+        <hr style={{ borderColor: "rgba(98, 98, 98, 0.2)" }} />
+      )}
+    </>
   );
 };
