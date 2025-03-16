253728c6a44f222cc42a4c4fa42c4483b53d7acc
Lorenzo Giacopuzzi <lorylucky04@gmail.com>
2025-03-16 16:05:52 +0100

    MDT-93: feat - added button on home cards

--- medit-frontend/src/components/home/MedicineComponent/MedicineComponent.tsx
+++ medit-frontend/src/components/home/MedicineComponent/MedicineComponent.tsx
@@ -5,8 +5,10 @@ import {
   ThemeProvider,
   Typography,
   Fade,
+  Button,
 } from "@mui/material";
 import { useLanguage } from "../../../contexts/LanguageContext";
+import { useNavigate } from "react-router-dom";
 import Ellipse from "../../../assets/icon/Check-Ellipse.svg";
 import Check from "../../../assets/icon/Check.svg";
 import Image from "../../../assets/icon/immagine.jpg";
@@ -27,12 +29,17 @@ export const MedicineComponent: React.FC<MedicineComponentProps> = ({
 }) => {
   const [isChecked, setIsChecked] = useState(false);
   const [bgColor, setBgColor] = useState("white");
+  const navigate = useNavigate();
 
   const toggleCheck = () => {
     setIsChecked((prev) => !prev);
     setBgColor((prev) =>
       prev === "white" ? "rgba(67, 134, 16, 0.8)" : "white"
     );
+  };
+
+  const handleCardClick = () => {
+    navigate("#");
   };
 
   const { language } = useLanguage();
@@ -52,15 +59,32 @@ export const MedicineComponent: React.FC<MedicineComponentProps> = ({
       <Box
         sx={{
           width: { xs: "85%", sm: "55%", md: "40%", lg: "30%", xl: "25%" },
-          height: { md: "12vh", xl: "16vh", xs: language === "it" ? "16vh" : "14vh" },
+          height: {
+            md: "12vh",
+            xl: "16vh",
+            xs: language === "it" ? "16vh" : "14vh",
+          },
           backgroundColor: bgColor,
           borderRadius: "10px",
           display: "flex",
           p: 2,
           boxShadow: "0px 0px 4px rgba(0, 0, 0, 0.25)",
           transition: "background-color 0.5s ease",
+          position: "relative",
         }}
       >
+        <Button
+          sx={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "75%",
+            height: "100%",
+            backgroundColor: "transparent",
+            zIndex: 2,
+          }}
+          onClick={handleCardClick}
+        />
         <Box
           sx={{
             width: { xs: "35%", sm: "22%", md: "23%", lg: "20%", xl: "25%" },
@@ -108,7 +132,8 @@ export const MedicineComponent: React.FC<MedicineComponentProps> = ({
                 variant="h5"
                 sx={{ fontWeight: "Medium", fontSize: "1.1rem" }}
               >
-                {translate(medication.program.toLowerCase())}, {medication.quantity} {translate("timesAday")}
+                {translate(medication.program.toLowerCase())},{" "}
+                {medication.quantity} {translate("timesAday")}
               </Typography>
             </ThemeProvider>
           </Box>
@@ -153,20 +178,18 @@ export const MedicineComponent: React.FC<MedicineComponentProps> = ({
           >
             <img src={Profile} alt="" width={"100%"} />
           </Box>
-          <Box>
-            <Box
-              sx={{ position: "relative", cursor: "pointer" }}
-              onClick={toggleCheck}
-            >
-              <Box>
-                <img src={Ellipse} alt="Ellipse" width={50} />
-              </Box>
-              <Fade in={isChecked} timeout={500}>
-                <Box sx={{ position: "absolute", top: 0, left: 2 }}>
-                  <img src={Check} alt="Check" width={46} />
-                </Box>
-              </Fade>
+          <Box
+            sx={{ position: "relative", cursor: "pointer" }}
+            onClick={toggleCheck}
+          >
+            <Box>
+              <img src={Ellipse} alt="Ellipse" width={50} />
             </Box>
+            <Fade in={isChecked} timeout={500}>
+              <Box sx={{ position: "absolute", top: 0, left: 2 }}>
+                <img src={Check} alt="Check" width={46} />
+              </Box>
+            </Fade>
           </Box>
         </Box>
       </Box>
